910527d92b3c604babbe7522f405fa5a74ee35cc
Dheeraj Nampally <nampally@motivationfactory.com>
2021-12-26 15:43:06 +0100

    Removed unnecessary using's

--- Program.cs
+++ Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Linq;
 using Newtonsoft.Json;
 
 namespace TranslateAzure
